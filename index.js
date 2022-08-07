9ecf05533e15190c2762a21198754d4547db6918
csslick <digitone@naver.com>
2022-08-07 22:35:35 +0900

    json output

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+const axios = require('axios')
 const dotenv = require('dotenv').config()
 const { Client } = require('@notionhq/client');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cors());
 
-app.get('/', async (req, res) => {
-  const lists = await getLists()
-  res.json(lists)
+app.get('/list', (req, res) => {
+  const options = {
+    method: 'POST',
+    url: `https://api.notion.com/v1/databases/${process.env.NOTION_DB_ID}/query`,
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+    },
+    data: {page_size: 100}
+  };
+  
+  // const lists = await getLists()
+  // res.json(lists)
+  axios
+  .request(options)
+  .then(function (lists) {
+    console.log(lists.data);
+    // return res.data;
+    res.json(lists.data)
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+  // res.json(lists)
 })
 
 app.listen(PORT, console.log(`Server run at ${PORT}`))
 
-
-const getLists = async () => {
-  const notion = new Client({ auth: process.env.NOTION_TOKEN });
-  const databaseId = process.env.NOTION_DB_ID;
-
-  const res = await notion.databases.query({
-  database_id: databaseId,
+const getLists = () => {
+  axios
+  .request(options)
+  .then(function (res) {
+    console.log(res.data);
+    return res.data;
+  })
+  .catch(function (error) {
+    console.error(error);
   });
-  // console.log(res);
-  return res;
 }
